9055e38c1e4570e5f123120f3469f9cf22aa14ac
José Rodrigo Moreno López <60509841+NoSoyRo@users.noreply.github.com>
2022-09-14 13:09:05 -0500

    Update ejemplo.py

--- choques/optimizado/ejemplo.py
+++ choques/optimizado/ejemplo.py
@@ -1,7 +1,13 @@
 from choques import *
 
-L = [particula(1, np.array([-0.5,0]), np.array([1,0]), np.array([1,0]), 0.5, 0), 
-     particula(1, np.array([0.5,0]), np.array([-1,0]), np.array([-1,0]), 0.5, 1)] 
+# L = [particula(1, np.array([-3,0]), np.array([1,0]), 1, 0), 
+#      particula(1, np.array([0,0]), np.array([0,0]), 1, 1),
+#      particula(1, np.array([3,0]), np.array([-1,0]), 1, 2)] 
 
-exp = experimento(L, 2, 2)
-exp.realiza_el_experimento(5, 0.1)
+
+L = [particula(1, np.array([0,2]), np.array([0,-1]), 1, 0), 
+     particula(1, np.array([0,-2]), np.array([0,1]), 1, 1)]
+#      particula(1, np.array([3,0]), np.array([-1,0]), 1, 2)] 
+
+exp = experimento(L, 5, 5)
+exp.realiza_el_experimento(500, 0.1)
